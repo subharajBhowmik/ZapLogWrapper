8f81323238290025e5b60450626d891303805921
Subharaj <subharajbhowmik@gmail.com>
2021-10-07 12:30:33 +0530

    Base Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ZapLogWrapper
+
+go 1.16
+
+require go.uber.org/zap v1.19.1
---
+++ model/zapLogError.go
@@ -0,0 +1,39 @@
+package model
+
+import (
+	"ZapLogWrapper/utils"
+	"go.uber.org/zap"
+)
+
+type ZapLogError struct {
+	ErrSrc     string
+	ErrCode    string
+	ErrSubCode string
+	ErrCause   error
+	Message    string
+	ZapFields  []zap.Field
+}
+
+func BuildNewZapLogError(errSrc string, errCode string, errSubCode string, errCause error, additionalFields ...interface{}) *ZapLogError {
+	zapLogError := ZapLogError{
+		ErrSrc:     errSrc,
+		ErrCode:    errCode,
+		ErrSubCode: errSubCode,
+		ErrCause:   errCause,
+	}
+	zapLogError.BuildZapLogMessageAndFields(additionalFields)
+	return &zapLogError
+}
+
+func (l *ZapLogError) Error() string {
+	return l.Message
+}
+
+func (l *ZapLogError) BuildZapLogMessageAndFields(addedFields ...interface{}) {
+	l.Message, l.ZapFields = utils.GenerateZapLog(l.ErrSrc, l.ErrCode, l.ErrSubCode, l.ErrCause, addedFields...)
+}
+
+func (l *ZapLogError) WithError(alternateError error) *ZapLogError {
+	l.ErrCause = alternateError
+	return l
+}
